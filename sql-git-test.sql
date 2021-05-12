0d34aab828f06a7ebbb29529da2b6ce1f29b4890
Katelyn Rains <krains@tql.com>
2021-05-12 15:58:47 -0400

    Enhancements to file have been made

---
+++ sql-git-test.sql
@@ -0,0 +1,12 @@
+--Sales by customer after 1/1/2018
+Select
+c.Name CustomerName
+,sum(ol.price) ItemSales
+from
+Orders o
+join Customers c on o.CustomerId = c.Id
+join OrderLines ol on o.Id = ol.OrdersId
+where o.Date > '1-1-2018'
+group by
+c.Name
+order by sum(ol.Price) descenhancement made
